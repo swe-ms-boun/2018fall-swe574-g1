ded52c3265c5eace32d8bba57df74f229acf6e45
Ugurcan Akpulat <ugurcan.akpulat@gmail.com>
2018-11-10 16:28:54 +0300

    uncompleted development of memory creation

--- src/main/java/com/boun/swe/mnemosyne/repository/MemoryRepository.java
+++ src/main/java/com/boun/swe/mnemosyne/repository/MemoryRepository.java
@@ -1,6 +1,5 @@
 package com.boun.swe.mnemosyne.repository;
 
-import com.boun.swe.mnemosyne.model.Area;
 import com.boun.swe.mnemosyne.model.Memory;
 import com.boun.swe.mnemosyne.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,5 +17,4 @@ public interface MemoryRepository extends JpaRepository<User, Long> {
     //List<Memory> findMemoriesByLocation(String location);
 
     //List<Memory> findMemoriesByArea(Area area);
-
 }
--- src/main/java/com/boun/swe/mnemosyne/service/MemoryService.java
+++ src/main/java/com/boun/swe/mnemosyne/service/MemoryService.java
@@ -1,9 +1,11 @@
 package com.boun.swe.mnemosyne.service;
 
 import com.boun.swe.mnemosyne.model.Memory;
+import com.boun.swe.mnemosyne.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
@@ -14,5 +16,17 @@ public class MemoryService {
 
     public List<Memory> getMemories(String username) {
         return null;
+    }
+
+    public Memory createNewMemories(User user)
+    {
+        Memory newMemo = new Memory();
+
+    }
+
+    public void setMemory(@ModelAttribute("userForm") final Memory UserForm, User user)
+    {
+        memory.setUser(user);
+        memory.setDateFrom();
     }
 }
