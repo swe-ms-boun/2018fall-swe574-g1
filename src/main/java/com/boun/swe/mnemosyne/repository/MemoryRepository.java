dc204bc30ff96b8fc3a27fdad67652dee7309dce
Vedat Akses <vedatakses@gmail.com>
2018-11-10 13:09:23 +0300

    Add more entities

--- src/main/java/com/boun/swe/mnemosyne/repository/MemoryRepository.java
+++ src/main/java/com/boun/swe/mnemosyne/repository/MemoryRepository.java
@@ -1,12 +1,8 @@
 package com.boun.swe.mnemosyne.repository;
 
-import com.boun.swe.mnemosyne.model.Area;
-import com.boun.swe.mnemosyne.model.Memory;
 import com.boun.swe.mnemosyne.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface MemoryRepository extends JpaRepository<User, Long> {
